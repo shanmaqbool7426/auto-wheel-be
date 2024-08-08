784968f5cf59601b8da7706717109ac8307919b8
shanmaqbool <shanmaqbool12345@gmail.com>
2024-08-08 14:34:14 +0500

    db change

--- src/Utils/connectDB.js
+++ src/Utils/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const mongoURI = process.env.MONGODB_URI;
-    await mongoose.connect('mongodb://localhost:27017/auto-wheels');
+    await mongoose.connect('mongodb+srv://s-booking-app:S-booking-app@booking-app.xb4efkw.mongodb.net');
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err);
