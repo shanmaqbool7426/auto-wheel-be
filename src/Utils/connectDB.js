5e4a8a313a17d25ae933da50b8ed06dcd8bb90aa
Abdullah-Ansari1 <abd45015@gmail.com>
2024-08-11 07:54:53 -0700

    filters api modify

--- src/Utils/connectDB.js
+++ src/Utils/connectDB.js
@@ -1,10 +1,9 @@
 import mongoose from 'mongoose';
-// mongodb+srv://s-booking-app:S-booking-app@booking-app.xb4efkw.mongodb.net
 
 const connectDB = async () => {
   try {
     const mongoURI = process.env.MONGODB_URI;
-    await mongoose.connect('mongodb://127.0.0.1:27017/auto-wheels');
+    await mongoose.connect('mongodb+srv://s-booking-app:S-booking-app@booking-app.xb4efkw.mongodb.net');
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err);
