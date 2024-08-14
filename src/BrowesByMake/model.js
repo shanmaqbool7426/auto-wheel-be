730dcb142ee035e7a1539fd60ffdb781c09f6afc
shanmaqbool <shanmaqbool12345@gmail.com>
2024-08-13 23:19:53 -0700

    add data

--- src/BrowesByMake/model.js
+++ src/BrowesByMake/model.js
@@ -1,12 +1,27 @@
 import mongoose from 'mongoose';
 
-const BrowesByMakeSchema = mongoose.Schema(
-    {
-        companyImage: { type: String },
-        name: { type: String, required: true }
+const variantSchema = new mongoose.Schema({
+  name: { type: String, required: true }
+});
+
+const modelSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  variants: []
+});
+
+const BrowesByMakeSchema = new mongoose.Schema(
+  {
+    companyImage: { type: String },
+    name: { type: String, required: true },
+    type: {
+      type: String,
+     
+      required: true
     },
-    { timestamps: true }
+    models: [modelSchema]
+  },
+  { timestamps: true }
 );
 
-const BrowesByMake= mongoose.model('BrowesByMake', BrowesByMakeSchema)
-export default BrowesByMake
+const BrowesByMake = mongoose.model('BrowesByMake', BrowesByMakeSchema);
+export default BrowesByMake;
--- src/Utils/connectDB.js
+++ src/Utils/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const mongoURI = process.env.MONGODB_URI;
-    await mongoose.connect('mongodb://127.0.0.1:27017/auto-wheelce');
+    await mongoose.connect('mongodb://127.0.0.1:27017/auto-wheels');
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err);
