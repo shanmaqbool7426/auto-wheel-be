098cdc0e6a6c8c6158d102533a4e7f10ba084a38
shanmaqbool <shanmaqbool12345@gmail.com>
2024-10-27 21:32:19 -0700

    cors

--- src/Utils/connectDB.js
+++ src/Utils/connectDB.js
@@ -1,14 +1,12 @@
 import mongoose from 'mongoose';
-let Uri="mongodb+srv://s-booking-app:S-booking-app@booking-app.xb4efkw.mongodb.net"
-// let Uri='mongodb://127.0.0.1:27017/auto-wheels'
+// let Uri="mongodb+srv://s-booking-app:S-booking-app@booking-app.xb4efkw.mongodb.net"
+let Uri='mongodb://127.0.0.1:27017/auto-wheels'
 const connectDB = async () => {
   try {
     const mongoURI = process.env.MONGODB_URI;
     await mongoose.connect(Uri);
     console.log('MongoDB connected...');
   } catch (err) {
-
-    
     console.error(err);
     
     process.exit(1);
