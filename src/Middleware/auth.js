df7ba97b89e33fe2d2c88ae18e9e80de6ee890a1
shanmaqbool <shanmaqbool12345@gmail.com>
2024-11-03 10:33:34 -0800

    profile

--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -7,11 +7,12 @@ const protect = asyncHandler(async (req, res, next) => {
 
   let token = req.headers.authorization
   if (token) {
+    console.log('token',token)
     try {
       const decoded = jwt.verify(token, 'token-secret-scv');
 
-      console.log('decodeddecoded',decoded)
       req.user = await User.findById(decoded?.id).select('-password');
+      console.log('>>>>>>>>>>>>',req.user)
       // if (!req.user) {
       //   return responses.unauthorized(res, 'Not authorized, user not found');
       // }
