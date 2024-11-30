92102591e9bc5d566ec4cf97cf21d0a157a34a09
shanmaqbool <shanmaqbool12345@gmail.com>
2024-11-30 11:10:59 -0800

    changes in cat

--- src/Chat/route.js
+++ src/Chat/route.js
@@ -5,7 +5,7 @@ import { protect } from '../Middleware/auth.js';
 const router = express.Router();
 
 router.post('/send', protect, sendMessage);
-router.get('/conversation/:userId', protect, getConversation);
-router.get('/conversations', protect, getConversationList);
+router.get('/:conversationId', getConversation);
+router.get('/list/:userId', getConversationList);
 
 export default router;
