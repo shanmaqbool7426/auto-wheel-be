968562b75ddc81bc3e5f20e651cf042961f70ea4
Abdullah-Ansari1 <abd45015@gmail.com>
2025-01-29 00:18:01 +0500

    Add user dashboard overview stats endpoint for vehicles

--- src/Vehicle/route.js
+++ src/Vehicle/route.js
@@ -9,6 +9,7 @@ import {
   //   updateVehicle,
   //   deleteVehicle,
   getListVehicles,
+  getOverviewStats,
   getPopularVehicles,
   getPopularVehiclesByReviews,
   getSimilarVehicles,
@@ -25,7 +26,7 @@ const router = express.Router();
 router.put('/:vehicleId/toggle-featured', toggleFeaturedVehicle);
 router.get('/vehicles-by-type', getBrowseByVehicles);
 router.get('/get-popular-vehicles', getPopularVehicles);
-
+router.get('/get-user-dashboard-overview',protect,getOverviewStats);
 
 
 router.get('/get-popular-by-reviews-vehicles', getPopularVehiclesByReviews);
