26261134da07b12d50de639006163e4ffc783869
Abdullah-Ansari1 <abd45015@gmail.com>
2025-01-31 02:54:43 +0500

    top permoing api

--- src/Vehicle/route.js
+++ src/Vehicle/route.js
@@ -17,6 +17,7 @@ import {
   getVehiclesForAdmin,
   toggleFeaturedVehicle,
   updateVehicleStatus,
+  getTopPerformingPostsBySeller
   // imageUploader
 } from "./controller.js"
 import { protect } from "../Middleware/auth.js";
@@ -27,6 +28,7 @@ router.put('/:vehicleId/toggle-featured', toggleFeaturedVehicle);
 router.get('/vehicles-by-type', getBrowseByVehicles);
 router.get('/get-popular-vehicles', getPopularVehicles);
 router.get('/get-user-dashboard-overview',protect,getOverviewStats);
+router.get('/get-user-top-performing-posts',protect,getTopPerformingPostsBySeller);
 
 
 router.get('/get-popular-by-reviews-vehicles', getPopularVehiclesByReviews);
